606b7a86f1fd14350079c5472bf3425f80df3bd2
Mathurin BODY <mathurin.body@gmail.com>
2020-05-30 18:24:04 +0200

    scenario hypo

--- Assets/Creator Kit - RPG/Scripts/Gameplay/Quest.cs
+++ Assets/Creator Kit - RPG/Scripts/Gameplay/Quest.cs
@@ -120,7 +120,7 @@ namespace RPGM.Gameplay
         {
             foreach (var i in rewardItems)
             {
-                MessageBar.Show($"You collected: {i.name} x {i.count}");
+                MessageBar.Show($"Vous trouvez : {i.name} x {i.count}");
                 model.AddInventoryItem(i);
                 UserInterfaceAudio.OnCollect();
                 i.gameObject.SetActive(false);
